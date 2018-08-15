882d9ee7f839f980fc0d4d7a2aefacdf997d472a
Tyler <tyler@eeosk.com>
2018-08-15 15:22:20 -0700

    Add MM

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,31 @@ module.exports = {
   description: 'Documentation & Examples for Anymod',
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
-      { text: 'External', link: 'https://google.com' },
+      { text: 'Examples', link: '/examples/' },
+      { text: 'Module Monday', link: '/module-monday/' },
+      { text: 'Library', link: 'https://anymod.com/library' },
     ],
-    sidebar: [
-      '/',
-      '/examples/markdown.html',
-      ['/guide/', 'Explicit link text']
-    ],
+    sidebar: {
+      '/guide/': [
+        '/',
+        '/examples/markdown.html',
+        ['/guide/', 'Explicit link text']
+      ],
+      '/examples/': [
+        '/examples/markdown.html',
+      ],
+      '/module-monday/': [
+        '/module-monday/08.html',
+        '/module-monday/07.html',
+        '/module-monday/06.html',
+        '/module-monday/05.html',
+        '/module-monday/04.html',
+        '/module-monday/03.html',
+        '/module-monday/02.html',
+        '/module-monday/01.html',
+      ]
+    },
     lastUpdated: 'Last Updated', // string | boolean
     // https://vuepress.vuejs.org/default-theme-config/#algolia-search
     // algolia: {
@@ -20,23 +36,21 @@ module.exports = {
     // }
 
     // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'vuejs/vuepress',
-    // Customising the header label
-    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
-    repoLabel: 'Contribute!',
+    // repo: 'anymod/guide',
+    // repoLabel: 'Contribute',
 
     // Optional options for generating "Edit this page" link
 
     // if your docs are in a different repo from your main project:
-    docsRepo: 'vuejs/vuepress',
+    // docsRepo: 'anymod/guide',
     // if your docs are not at the root of the repo:
-    docsDir: 'docs',
+    // docsDir: '..',
     // if your docs are in a specific branch (defaults to 'master'):
     docsBranch: 'master',
     // defaults to false, set to true to enable
     editLinks: true,
     // custom text for edit link. Defaults to "Edit this page"
-    editLinkText: 'Help us improve this page!'
+    editLinkText: 'Help us improve this page'
   },
   markdown: {
     lineNumbers: true
---
+++ docs/module-monday/index.md
@@ -0,0 +1,24 @@
+---
+title: Module Monday
+lang: en-US
+---
+
+# Module Monday
+
+Module Monday is a weekly collection of open-source mods you can use to quickly add features to your website or web app.
+
+Get your weekly mod fix in any of the following ways:
+
+- By following on [Dev.to](https://dev.to/tyrw) or [Medium](https://medium.com/anymod)
+- By signing up for [Anymod](https://anymod.com)
+- By subscribing to the [mailing list](https://anymod.us17.list-manage.com/subscribe?u=8d6f80723bc1252c9882e7d3f&id=b334c54b7e)
+
+### Open source
+
+We support developers and open source.
+
+Module Mondays are one way we give back to the developer community to help foster a more open, inclusive web.
+
+### Contributing
+
+If you want to contribute mods or ideas so that other developers can benefit, you can [request mods](https://guide.anymod.com/v1/community/requests.html) or [submit a mod](https://guide.anymod.com/v1/community/contributing.html).
