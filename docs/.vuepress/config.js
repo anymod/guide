63bdc85787c75a15f73e41dd3d8e8661f2e3090c
Tyler <tyler@anymod.com>
2019-12-16 09:59:43 -0800

    Added MM 67

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -137,6 +137,7 @@ module.exports = {
         // '/examples/recaptcha.html',
       ],
       "/module-monday/": [
+        "/module-monday/67.html",
         "/module-monday/66.html",
         "/module-monday/65.html",
         "/module-monday/64.html",
