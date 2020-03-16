4754db9baeb1abf342139e6c6a4eb2e5eb035370
Tyler <tyler@anymod.com>
2020-03-16 11:02:31 -0700

    Add MM 76 and merge auto commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -137,6 +137,7 @@ module.exports = {
         // '/examples/recaptcha.html',
       ],
       "/module-monday/": [
+        "/module-monday/76.html",
         "/module-monday/75.html",
         "/module-monday/74.html",
         "/module-monday/73.html",
